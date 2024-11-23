c714cb6a584881e8c0288fb809695a56096aebbe
fmoreira <fabianomoreira.ti@gmail.com>
2024-11-23 14:47:33 -0300

    feat:footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import { cardColaboratorComponent } from "/components/card-colaborator/card-colaborator.js";
+import { footerComponent } from "/components/footer-component/footer-component.js";
 
 cardColaboratorComponent();
+footerComponent()
